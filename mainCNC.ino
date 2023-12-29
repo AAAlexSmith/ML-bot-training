9b00dd19724f48563c7a13da06be8f7ece487098
Alexander Smith <139652601+AAAlexSmith@users.noreply.github.com>
2023-12-28 22:53:25 -0600

    Update mainCNC.ino
    updated with some base code for the stepper motor controls. Still need to figure out enable pins.

--- mainCNC.ino
+++ mainCNC.ino
@@ -1,22 +1,76 @@
-String incomingByte ;    
+// testing a stepper motor with a Pololu A4988 driver board or equivalent
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+// this version uses millis() to manage timing rather than delay()
+// and the movement is determined by a pair of momentary push switches
+// press one and it turns CW, press the other and it turns CCW
+
+byte directionPin = 9;
+byte stepPin = 8;
+
+byte buttonCWpin = 10;
+byte buttonCCWpin = 11;
+
+boolean buttonCWpressed = false;
+boolean buttonCCWpressed = false;
+
+byte ledPin = 13;
+
+unsigned long curMillis;
+unsigned long prevStepMillis = 0;
+unsigned long millisBetweenSteps = 25; // milliseconds
+
+void setup() { 
+
+     Serial.begin(9600);
+     Serial.println("Starting Stepper Demo with millis()");
+
+     pinMode(directionPin, OUTPUT);
+     pinMode(stepPin, OUTPUT);
+     pinMode(ledPin, OUTPUT);
+     
+     pinMode(buttonCWpin, INPUT_PULLUP);
+     pinMode(buttonCCWpin, INPUT_PULLUP);
+     
 }
-void loop() {
-  if (Serial.available() > 0) {
-  incomingByte = Serial.readStringUntil('\n');
-    if (incomingByte == "on") {
-      digitalWrite(LED_BUILTIN, HIGH);
-      Serial.write("Led on");
+
+void loop() { 
+    
+    curMillis = millis();
+    readButtons();
+    actOnButtons();
+    
+}
+
+void readButtons() {
+    
+    buttonCCWpressed = false;
+    buttonCWpressed = false;
+    
+    if (digitalRead(buttonCWpin) == LOW) {
+        buttonCWpressed = true;
     }
-    else if (incomingByte == "off") {
-      digitalWrite(LED_BUILTIN, LOW);
-      Serial.write("Led off");
+    if (digitalRead(buttonCCWpin) == LOW) {
+        buttonCCWpressed = true;
     }
-    else{
-     Serial.write("invald input");
+}
+
+void actOnButtons() {
+    if (buttonCWpressed == true) {
+        digitalWrite(directionPin, LOW);
+        singleStep();
     }
-  }
+    if (buttonCCWpressed == true) {
+        digitalWrite(directionPin, HIGH);
+        singleStep();
+    }
+}
+
+void singleStep() {
+    if (curMillis - prevStepMillis >= millisBetweenSteps) {
+            // next 2 lines changed 28 Nov 2018
+        //prevStepMillis += millisBetweenSteps;
+        prevStepMillis = curMillis;
+        digitalWrite(stepPin, HIGH);
+        digitalWrite(stepPin, LOW);
+    }
 }
